a861b244cad2104f6468a90054989994e43d2682
EndUser <julian.abarca@gmail.com>
2015-01-30 15:24:21 -0600

    Ejemplo de arreglos. Comprobar si una palabra es palindromo

---
+++ ejercicio1/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: EndUser
+ *
+ * Created on 30 de enero de 2015, 03:23 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
